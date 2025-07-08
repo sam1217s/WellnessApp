3e0380351a093dd472f16479ff66b7e1e06eab33
sam1217s <1705alejogomez@gmail.com>
2025-07-07 22:22:46 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       targets: [
         {
           src: 'public/_redirects',
-          dest: '.' // se copia a dist/
+          dest: '.'  // Copia el archivo a /dist
         }
       ]
     })
